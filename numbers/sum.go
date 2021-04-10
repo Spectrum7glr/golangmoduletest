4cf00cedbf00f0565ae1c29015f176451ca2f82e
Lorenzo Di Marco <lorenzo.dimarco@outlook.com>
2021-04-10 05:53:04 +0200

    Add function

--- numbers/sum.go
+++ numbers/sum.go
@@ -3,3 +3,15 @@ package numbers
 func Add2terms(i int, j int) int {
 	return i + j
 }
+
+func Add(numbers ...int) int {
+
+	sum := 0
+
+	for _, v := range numbers {
+		sum += v
+	}
+
+	return sum
+
+}
